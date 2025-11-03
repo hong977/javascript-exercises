a1ff6143d8ec79ee6317714e32dc9322f5dc5e20
hong977 <youjinghong2006@gmail.com>
2025-11-03 15:46:34 +0800

    Done until question 11

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if ( (a<0 || b<0) || (typeof(a) != "number" || 
+    typeof(b) != "number") ||
+    (!Number.isInteger(a) || !Number.isInteger(b)))
+    {
+        return "ERROR";
+    }
 
+    let start;
+    let end;
+    if (a < b){
+        start = a;
+        end = b;
+    }
+    else {
+        start = b;
+        end = a;
+    }
+
+    for (let i = start+1; i <= end ; i++){
+        start += i;
+    }
+    return start;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = Number(((fahrenheit - 32) * (5/9)).toFixed(1)); 
+  return celcius == 0? 0:celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = Number(((celcius * (9/5)) + 32).toFixed(1));
+  return fahrenheit == 0? 0 : fahrenheit;
 };
 
 // Do not edit below this line
